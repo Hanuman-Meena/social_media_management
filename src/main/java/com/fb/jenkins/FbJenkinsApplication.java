a95c88dd42480151b505cc77de5bcf6daf1c3784
Hanuman Meena <hanuman.meena.0698@gmail.com>
2023-10-26 21:24:53 +0530

    initial commit

---
+++ src/main/java/com/fb/jenkins/FbJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.fb.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbJenkinsApplication.class, args);
+	}
+
+}
