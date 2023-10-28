e3cfc3a2d86d313b870687a6e77dfa8215e61943
Hanuman Meena <hanuman.meena.0698@gmail.com>
2023-10-28 14:50:49 +0530

    testing

--- src/main/java/com/fb/jenkins/Controllers/HomeController.java
+++ src/main/java/com/fb/jenkins/Controllers/HomeController.java
@@ -14,7 +14,7 @@ public class HomeController {
 	@ResponseBody
 	public String home() {
 		
-		return "Hello World! This is woro Social Medial Management Tool Application!";
+		return "This is woro Social Medial Management Tool Application!";
 	}
 	
 	@GetMapping("/welcome/{name}")
