93570b9a8c45ee05b1c418229e70d21bff7e1509
Hanuman Meena <hanuman.meena.0698@gmail.com>
2023-10-28 13:41:35 +0530

    added new handler

--- src/main/java/com/fb/jenkins/Controllers/HomeController.java
+++ src/main/java/com/fb/jenkins/Controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.fb.jenkins.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,9 @@ public class HomeController {
 		return "Hello World! This is woro media application!";
 	}
 	
-	@GetMapping("/welcome")
+	@GetMapping("/welcome/{name}")
 	@ResponseBody
-	public String welcome() {
-		return "Welcome to woro media!!";
+	public String welcome(@PathVariable("name")String name) {
+		return "Hi "+ name + "! Welcome to woro media!!";
 	}
 }
