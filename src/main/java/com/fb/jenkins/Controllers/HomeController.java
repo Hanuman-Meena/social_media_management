7bd44cef91ab3428d82da6a49d2f3a4fa8b37d31
Hanuman Meena <hanuman.meena.0698@gmail.com>
2023-10-30 13:22:36 +0530

    dependency injected

--- src/main/java/com/fb/jenkins/Controllers/HomeController.java
+++ src/main/java/com/fb/jenkins/Controllers/HomeController.java
@@ -2,11 +2,9 @@ package com.fb.jenkins.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	
@@ -17,16 +15,5 @@ public class HomeController {
 		return "Hi! Welcome to Woro Social Media Management Application!";
 	}
 	
-	@GetMapping("/home")
-	@ResponseBody
-	public String home() {
-		
-		return "welcome here!";
-	}
 	
-	@GetMapping("/welcome/{name}")
-	@ResponseBody
-	public String welcome(@PathVariable("name")String name) {
-		return "Hi! "+ name + "! Welcome to woro media family!!";
-	}
 }
